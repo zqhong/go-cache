d4b87f31caca9c995c441c20cc07b07c6db5fdfb
zqhong <zqh0513@gmail.com>
2019-06-13 13:40:39 +0800

    added: 添加 Exists 方法

--- cache.go
+++ cache.go
@@ -37,17 +37,23 @@ func (c *cache) Put(k interface{}, x interface{}, d time.Duration) {
 }
 
 // Get item in Cache, and drop when it expired
-func (c *cache) Get(k interface{}) interface{} {
-	v, ok := c.mapping.Load(k)
+func (c *cache) Get(key interface{}) interface{} {
+	v, ok := c.mapping.Load(key)
 	if !ok {
 		return nil
 	}
 	i := v.(*item)
 	if time.Since(i.Expired) > 0 {
-		c.mapping.Delete(k)
+		c.mapping.Delete(key)
 		return nil
 	}
 	return i.Payload
+}
+
+// Exists method is used to check if a given key exists
+func (c *cache) Exists(key interface{}) bool {
+	_, ok := c.mapping.Load(key)
+	return ok
 }
 
 func (c *cache) SetCleanupInterval(interval time.Duration) {
--- cache_test.go
+++ cache_test.go
@@ -13,17 +13,23 @@ func TestCacheBasic(t *testing.T) {
 	if none != nil {
 		t.Error("should receive nil")
 	}
+	if c.Exists("not-found") {
+		t.Error("The key named 'not-found' should not exist.")
+	}
 
 	c.Put("str", "go-cache", 100*time.Millisecond)
 	s := c.Get("str")
 	if s.(string) != "go-cache" {
-		t.Error("should receive 'go-cache'")
+		t.Error("it should receive 'go-cache'")
+	}
+	if !c.Exists("str") {
+		t.Error("The key named 'str' should exist.")
 	}
 
 	c.Put("int", 1, 100*time.Millisecond)
 	i := c.Get("int")
 	if i.(int) != 1 {
-		t.Error("should receive 1")
+		t.Error("it should receive 1")
 	}
 }
 
